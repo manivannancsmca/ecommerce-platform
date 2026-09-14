83560e8e83d00dbf466e32cd1f280b242ebaf15a
manivannancsmca <manivannancsmca@gmail.com>
2026-09-14 17:30:41 +0530

    BusinessException added

--- user-service/src/main/java/com/ecommerce/user/exception/BusinessException.java
+++ user-service/src/main/java/com/ecommerce/user/exception/BusinessException.java
@@ -1,5 +1,21 @@
+// src/main/java/com/ecommerce/user/exception/BusinessException.java
 package com.ecommerce.user.exception;
 
-public class BusinessException {
+public class BusinessException extends RuntimeException {
 
+    private final String errorCode;
+
+    public BusinessException(String errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(String errorCode, String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
 }
