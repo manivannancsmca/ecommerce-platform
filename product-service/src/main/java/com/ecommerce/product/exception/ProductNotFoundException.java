c520b70f4f1c5cc64c47990106b7475b63eff484
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:09:27 +0530

    ProductNotFoundException added

--- product-service/src/main/java/com/ecommerce/product/exception/ProductNotFoundException.java
+++ product-service/src/main/java/com/ecommerce/product/exception/ProductNotFoundException.java
@@ -1,5 +1,15 @@
+// src/main/java/com/ecommerce/product/exception/ProductNotFoundException.java
 package com.ecommerce.product.exception;
 
-public class ProductNotFoundException {
+import java.util.UUID;
 
+public class ProductNotFoundException extends BusinessException {
+
+    public ProductNotFoundException(UUID id) {
+        super("PRODUCT_NOT_FOUND", "Product not found with id: " + id);
+    }
+
+    public ProductNotFoundException(String sku) {
+        super("PRODUCT_NOT_FOUND", "Product not found with SKU: " + sku);
+    }
 }
