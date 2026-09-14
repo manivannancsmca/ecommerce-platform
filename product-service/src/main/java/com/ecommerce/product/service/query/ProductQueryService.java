81149688140de767d52e43a8245f087520a0a5aa
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:14:47 +0530

    ProductQueryService added

--- product-service/src/main/java/com/ecommerce/product/service/query/ProductQueryService.java
+++ product-service/src/main/java/com/ecommerce/product/service/query/ProductQueryService.java
@@ -1,5 +1,13 @@
+// src/main/java/com/ecommerce/product/service/query/ProductQueryService.java
 package com.ecommerce.product.service.query;
 
-public class ProductQueryService {
+import com.ecommerce.product.dto.request.ProductSearchRequest;
+import com.ecommerce.product.dto.response.ProductResponse;
+import com.ecommerce.product.dto.response.ProductSearchResponse;
 
+import java.util.UUID;
+
+public interface ProductQueryService {
+    ProductResponse getProductById(UUID id);
+    ProductSearchResponse searchProducts(ProductSearchRequest request);
 }
