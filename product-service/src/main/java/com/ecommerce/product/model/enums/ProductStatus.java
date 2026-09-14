2cb9873eff76a892835d04236d8c316584539b53
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:01:37 +0530

    ProductStatus added

--- product-service/src/main/java/com/ecommerce/product/model/enums/ProductStatus.java
+++ product-service/src/main/java/com/ecommerce/product/model/enums/ProductStatus.java
@@ -1,5 +1,9 @@
+// src/main/java/com/ecommerce/product/model/enums/ProductStatus.java
 package com.ecommerce.product.model.enums;
 
 public enum ProductStatus {
-
+    ACTIVE,
+    INACTIVE,
+    OUT_OF_STOCK,
+    DISCONTINUED
 }
