f2372a74f902c3db8bf14257504ef3e8bc618e97
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:05:15 +0530

    UpdateStockRequest added

--- product-service/src/main/java/com/ecommerce/product/dto/request/UpdateStockRequest.java
+++ product-service/src/main/java/com/ecommerce/product/dto/request/UpdateStockRequest.java
@@ -1,5 +1,15 @@
+// src/main/java/com/ecommerce/product/dto/request/UpdateStockRequest.java
 package com.ecommerce.product.dto.request;
 
-public class UpdateStockRequest {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
-}
+public record UpdateStockRequest(
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    Integer quantity,
+
+    @NotNull(message = "Increment flag is required")
+    Boolean increment
+) {}
