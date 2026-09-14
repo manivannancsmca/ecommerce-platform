74ff5c84c1e296d5a4513e04c66147571e56e811
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:18:24 +0530

    CorrelationIdHolder added

--- product-service/src/main/java/com/ecommerce/product/common/CorrelationIdHolder.java
+++ product-service/src/main/java/com/ecommerce/product/common/CorrelationIdHolder.java
@@ -1,5 +1,23 @@
+// src/main/java/com/ecommerce/product/common/CorrelationIdHolder.java
 package com.ecommerce.product.common;
 
-public class CorrelationIdHolder {
+import org.slf4j.MDC;
 
+public final class CorrelationIdHolder {
+
+    private static final String KEY = "correlationId";
+
+    private CorrelationIdHolder() {}
+
+    public static String getCorrelationId() {
+        return MDC.get(KEY);
+    }
+
+    public static void setCorrelationId(String correlationId) {
+        if (correlationId != null) MDC.put(KEY, correlationId);
+    }
+
+    public static void clear() {
+        MDC.remove(KEY);
+    }
 }
