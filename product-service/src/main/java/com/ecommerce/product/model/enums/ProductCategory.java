2f56ade52aea70612650a76c86ce9504f1129ac6
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:01:54 +0530

    ProductCategory added

--- product-service/src/main/java/com/ecommerce/product/model/enums/ProductCategory.java
+++ product-service/src/main/java/com/ecommerce/product/model/enums/ProductCategory.java
@@ -1,5 +1,15 @@
+// src/main/java/com/ecommerce/product/model/enums/ProductCategory.java
 package com.ecommerce.product.model.enums;
 
 public enum ProductCategory {
-
+    ELECTRONICS,
+    CLOTHING,
+    BOOKS,
+    HOME_AND_KITCHEN,
+    SPORTS,
+    BEAUTY,
+    TOYS,
+    AUTOMOTIVE,
+    GROCERY,
+    OTHER
 }
