a1cf1db9c210b0b88940824a46b8817e8ad8db9f
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:10:06 +0530

    DuplicateSkuException added

--- product-service/src/main/java/com/ecommerce/product/exception/DuplicateSkuException.java
+++ product-service/src/main/java/com/ecommerce/product/exception/DuplicateSkuException.java
@@ -1,5 +1,9 @@
+// src/main/java/com/ecommerce/product/exception/DuplicateSkuException.java
 package com.ecommerce.product.exception;
 
-public class DuplicateSkuException {
+public class DuplicateSkuException extends BusinessException {
 
+    public DuplicateSkuException(String sku) {
+        super("DUPLICATE_SKU", "A product with SKU '" + sku + "' already exists");
+    }
 }
