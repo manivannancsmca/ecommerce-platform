30642b22f8dec9feb74a0796297e42ed2827be97
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:08:46 +0530

    ProductSearchRepository added

--- product-service/src/main/java/com/ecommerce/product/repository/read/ProductSearchRepository.java
+++ product-service/src/main/java/com/ecommerce/product/repository/read/ProductSearchRepository.java
@@ -1,5 +1,12 @@
+// src/main/java/com/ecommerce/product/repository/read/ProductSearchRepository.java
 package com.ecommerce.product.repository.read;
 
-public class ProductSearchRepository {
+import com.ecommerce.product.model.document.ProductDocument;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
+    // Basic CRUD is inherited (save, findById, deleteById, existsById, count).
+    // Complex search queries are built programmatically in ProductQueryServiceImpl.
 }
