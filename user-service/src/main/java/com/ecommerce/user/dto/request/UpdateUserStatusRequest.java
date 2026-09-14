295a73ef67ffc76819f8ad697657e5f4bf0b28cf
manivannancsmca <manivannancsmca@gmail.com>
2026-09-14 17:27:17 +0530

    UpdateUserStatusRequest added

--- user-service/src/main/java/com/ecommerce/user/dto/request/UpdateUserStatusRequest.java
+++ user-service/src/main/java/com/ecommerce/user/dto/request/UpdateUserStatusRequest.java
@@ -1,5 +1,11 @@
+// src/main/java/com/ecommerce/user/dto/request/UpdateUserStatusRequest.java
 package com.ecommerce.user.dto.request;
 
-public class UpdateUserStatusRequest {
+import com.ecommerce.user.model.enums.UserStatus;
+import jakarta.validation.constraints.NotNull;
 
-}
+public record UpdateUserStatusRequest(
+
+    @NotNull(message = "Status is required")
+    UserStatus status
+) {}
