274ab60396c4db707eb6f03ba7de7b754742f129
manivannancsmca <manivannancsmca@gmail.com>
2026-09-14 17:31:16 +0530

    EmailAlreadyExistsException added

--- user-service/src/main/java/com/ecommerce/user/exception/EmailAlreadyExistsException.java
+++ user-service/src/main/java/com/ecommerce/user/exception/EmailAlreadyExistsException.java
@@ -1,5 +1,9 @@
+// src/main/java/com/ecommerce/user/exception/EmailAlreadyExistsException.java
 package com.ecommerce.user.exception;
 
-public class EmailAlreadyExistsException {
+public class EmailAlreadyExistsException extends BusinessException {
 
+    public EmailAlreadyExistsException(String email) {
+        super("EMAIL_ALREADY_EXISTS", "A user with email '" + email + "' already exists");
+    }
 }
