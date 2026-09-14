81b9224859ddbe6ad9ee908f27b0a10c68262d13
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:19:47 +0530

    ProductServiceApplication updated

--- product-service/src/main/java/com/ecommerce/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/ecommerce/product/ProductServiceApplication.java
@@ -3,7 +3,12 @@ package com.ecommerce.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
+@EnableScheduling
+@EnableElasticsearchRepositories(basePackages = "com.ecommerce.product.repository.read")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
