275e8c19fe9a34904774bc753d6fa687f0e1fed9
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:09:49 +0530

    InsufficientStockException added

--- product-service/src/main/java/com/ecommerce/product/exception/InsufficientStockException.java
+++ product-service/src/main/java/com/ecommerce/product/exception/InsufficientStockException.java
@@ -1,5 +1,13 @@
+// src/main/java/com/ecommerce/product/exception/InsufficientStockException.java
 package com.ecommerce.product.exception;
 
-public class InsufficientStockException {
+import java.util.UUID;
 
+public class InsufficientStockException extends BusinessException {
+
+    public InsufficientStockException(UUID productId, int requested, int available) {
+        super("INSUFFICIENT_STOCK",
+                String.format("Insufficient stock for product %s: requested=%d, available=%d",
+                        productId, requested, available));
+    }
 }
