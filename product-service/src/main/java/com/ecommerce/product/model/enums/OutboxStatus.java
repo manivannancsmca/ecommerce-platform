fe11f4d4b0aee14f087195430078a2a6721b8914
manivannancsmca <manivannancsmca@gmail.com>
2026-09-15 01:02:16 +0530

    OutboxStatus added

--- product-service/src/main/java/com/ecommerce/product/model/enums/OutboxStatus.java
+++ product-service/src/main/java/com/ecommerce/product/model/enums/OutboxStatus.java
@@ -1,5 +1,8 @@
+// src/main/java/com/ecommerce/product/model/enums/OutboxStatus.java
 package com.ecommerce.product.model.enums;
 
 public enum OutboxStatus {
-
+    PENDING,
+    SENT,
+    FAILED
 }
